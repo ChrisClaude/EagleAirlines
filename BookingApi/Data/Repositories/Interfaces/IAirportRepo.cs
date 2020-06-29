627fc35d73b14276936e91dabaf0256264302982
ChClaude <christ.tchambila@gmail.com>
2020-06-30 01:48:26 +0200

    Repository pattern implemented for the Airport entity

---
+++ BookingApi/Data/Repositories/Interfaces/IAirportRepo.cs
@@ -0,0 +1,18 @@
+﻿using BookingApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingApi.Data.Repositories
+{
+    public interface IAirportRepo
+    {
+        bool SaveChanges();
+        IEnumerable<Airport> GetAllAirports();
+        Airport GetAirportById(int id);
+        void CreateAirport(Airport airport);
+        void UpdateAirport(Airport airport);
+        void DeleteAirport(Airport airport);
+    }
+}
---
+++ BookingApi/Data/Repositories/SqlAirportRepo.cs
@@ -0,0 +1,59 @@
+﻿using BookingApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingApi.Data.Repositories
+{
+    public class SqlAirportRepo : IAirportRepo
+    {
+        public SqlAirportRepo(BookingContext context)
+        {
+            _context = context;
+        }
+
+        public BookingContext _context { get; }
+
+        public void CreateAirport(Airport airport)
+        {
+            if (airport == null)
+            {
+                throw new ArgumentNullException(nameof(airport));
+            }
+
+            _context.Airports.Add(airport);
+        }
+
+        public IEnumerable<Airport> GetAllAirports()
+        {
+            return _context.Airports.ToList();
+        }
+
+        public Airport GetAirportById(int id)
+        {
+            return _context.Airports.FirstOrDefault(p => p.ID == id);
+        }
+
+        public void UpdateAirport(Airport airport)
+        {
+            // Nothing
+        }
+
+        public void DeleteAirport(Airport airport)
+        {
+            if (airport == null)
+            {
+                throw new ArgumentNullException(nameof(airport));
+            }
+
+            _context.Airports.Remove(airport);
+        }
+
+        public bool SaveChanges()
+        {
+            return _context.SaveChanges() >= 0;
+        }
+    }
+}
