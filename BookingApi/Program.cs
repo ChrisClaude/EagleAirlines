47fb1394e0d778347337c3153f3b272df1fc1c62
ChClaude <christ.tchambila@gmail.com>
2020-07-08 00:29:07 +0200

    Logging added

--- BookingApi/Program.cs
+++ BookingApi/Program.cs
@@ -42,6 +42,11 @@ namespace BookingApi
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
